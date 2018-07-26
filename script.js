b7a79dc84467d5cd22d6893739a9330f231ca6de
MathyMcMatherson <danielschneidermath@gmail.com>
2018-07-26 02:52:45 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//Step 1: Declare your variables to select elements in the HTML
+
+
+//Step 2: Declare your variable to keep track of how many times you've clicked
+
+
+//Step 3: Add an event listener to the 'click me' button
+
+
+//Step 4: Within that event listener:
+//  - Increase the number of clicks
+//  - If there are 5 clicks: change the image to a chicken and display "I'm a Chicken!"
+//  - If there are 10 clicks: change the image to a raptor and display "I'm a Raptor!"
+//  - If there are 20 clicks: change the image to an immortal and display "I'm an Immortal!"
+
+//Chicken Image: https://www.shareicon.net/data/256x256/2017/05/06/885719_shell_512x512.png
+//Raptor Image: https://cdn3.bigcommerce.com/s-5jd6v8l/products/604/images/5511/Dinosaurs_Raptor__60328.1510085540.500.750.jpg?c=2
+//Immortal Image: (You Find one that you like!)
